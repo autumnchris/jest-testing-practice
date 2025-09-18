269136c95ca0c9da7a1bf7943f30b422806fc67d
autumnchris <autumncbullard@gmail.com>
2025-09-18 16:37:51 -0400

    Update calculator functions

--- calculator.js
+++ calculator.js
@@ -1,7 +1,7 @@
 function add(num1, num2) {
 
   if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-    return 'ERROR: The \"num1\" parameter and the \"num2\" parameter must be of type number.';
+    throw new Error('The "num1" parameter and the "num2" parameter must be of type number.');
   }
   else {
     return num1 + num2;
@@ -11,7 +11,7 @@ function add(num1, num2) {
 function subtract(num1, num2) {
 
   if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-    return 'ERROR: The \"num1\" parameter and the \"num2\" parameter must be of type number.';
+    throw new Error('The "num1" parameter and the "num2" parameter must be of type number.');
   }
   else {
     return num1 - num2;
@@ -21,7 +21,7 @@ function subtract(num1, num2) {
 function multiply(num1, num2) {
 
   if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-    return 'ERROR: The \"num1\" parameter and the \"num2\" parameter must be of type number.';
+    throw new Error('The "num1" parameter and the "num2" parameter must be of type number.');
   }
   else {
     return num1 * num2;
@@ -31,10 +31,10 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
 
   if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-    return 'ERROR: The \"num1\" parameter and the \"num2\" parameter must be of type number.';
+    throw new Error('The "num1" parameter and the "num2" parameter must be of type number.');
   }
   else if (num2 === 0) {
-    return 'ERROR: The \"num2\" parameter cannot have a value of 0.';
+    throw new Error('The "num2" parameter cannot have a value of 0.');
   }
   else {
     return num1 / num2;
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -5,104 +5,104 @@ const {
   divide
 } = require('../calculator');
 
-describe('\"calculator\" object tests', () => {
-
-  describe('\"add\" function tests', () => {
-    test('add(2, 2) should return a value', () => {
-      expect(add(2, 2)).toBeDefined();
+describe('Calculator tests.', () => {
+  describe('Add function tests.', () => {
+    describe('Valid input tests.', () => {
+      test('Add two positive integers.', () => {
+        expect(add(2, 2)).toBe(4);
+      });
+      test('Add a positive integer and a negative integer.', () => {
+        expect(add(2, -4)).toBe(-2);
+      });
+      test('Add an integer and a decimal number.', () => {
+        expect(add(0.2, 4)).toBe(4.2);
+      });
     });
-    test('add(2, 2) should return a number', () => {
-      expect(typeof add(2, 2)).toBe('number');
-    });
-    test('add(2, 2) should return the number 4', () => {
-      expect(add(2, 2)).toBe(4);
-    });
-    test('add(2, -4) should return the number -2', () => {
-      expect(add(2, -4)).toBe(-2);
-    });
-    test('add(0.2, 4) should return the number 4.2', () => {
-      expect(add(0.2, 4)).toBe(4.2);
-    });
-    test('add(\'2\', 2) should return an error', () => {
-      expect(add('2', 2)).toBe('ERROR: The \"num1\" parameter and the \"num2\" parameter must be of type number.');
-    });
-    test('add(2, \'2\') should return an error', () => {
-      expect(add(2, '2')).toBe('ERROR: The \"num1\" parameter and the \"num2\" parameter must be of type number.');
-    });
-  });
-
-  describe('\"subtract\" function tests', () => {
-    test('subtract(2, 2) should return a value', () => {
-      expect(subtract(2, 2)).toBeDefined();
-    });
-    test('subtract(2, 2) should return a number', () => {
-      expect(typeof subtract(2, 2)).toBe('number');
-    });
-    test('subtract(2, 2) should return the number 0', () => {
-      expect(subtract(2, 2)).toBe(0);
-    });
-    test('subtract(2, -4) should return the number 6', () => {
-      expect(subtract(2, -4)).toBe(6);
-    });
-    test('subtract(0.2, 4) should return the number -3.8', () => {
-      expect(subtract(0.2, 4)).toBe(-3.8);
-    });
-    test('subtract(\'2\', 2) should return an error', () => {
-      expect(subtract('2', 2)).toBe('ERROR: The \"num1\" parameter and the \"num2\" parameter must be of type number.');
-    });
-    test('subtract(2, \'2\') should return an error', () => {
-      expect(subtract(2, '2')).toBe('ERROR: The \"num1\" parameter and the \"num2\" parameter must be of type number.');
+    describe('Invalid input tests.', () => {
+      test('Throws an error if the first number is of type string.', () => {
+        expect(() => add('2', 2)).toThrow('The "num1" parameter and the "num2" parameter must be of type number.');
+      });
+      test('Throws an error if the second number is of type string.', () => {
+        expect(() => add(2, '2')).toThrow('The "num1" parameter and the "num2" parameter must be of type number.');
+      });
+      test('Throws an error if one or more inputs are undefined.', () => {
+        expect(() => add(2)).toThrow('The "num1" parameter and the "num2" parameter must be of type number.');
+      });
     });
   });
-
-  describe('\"multiply\" function tests', () => {
-    test('multiply(2, 2) should return a value', () => {
-      expect(multiply(2, 2)).toBeDefined();
+  describe('Subtract function tests.', () => {
+    describe('Valid input tests.', () => {
+      test('Subtract two positive integers.', () => {
+        expect(subtract(2, 2)).toBe(0);
+      });
+      test('Subtract a positive integer and a negative integer.', () => {
+        expect(subtract(2, -4)).toBe(6);
+      });
+      test('Subtract an integer and a decimal number.', () => {
+        expect(subtract(0.2, 4)).toBe(-3.8);
+      });
     });
-    test('multiply(2, 2) should return a number', () => {
-      expect(typeof multiply(2, 2)).toBe('number');
-    });
-    test('multiply(2, 2) should return the number 4', () => {
-      expect(multiply(2, 2)).toBe(4);
-    });
-    test('multiply(2, -4) should return the number -8', () => {
-      expect(multiply(2, -4)).toBe(-8);
-    });
-    test('multiply(0.2, 4) should return the number 0.8', () => {
-      expect(multiply(0.2, 4)).toBe(0.8);
-    });
-    test('multiply(\'2\', 2) should return an error', () => {
-      expect(multiply('2', 2)).toBe('ERROR: The \"num1\" parameter and the \"num2\" parameter must be of type number.');
-    });
-    test('multiply(2, \'2\') should return an error', () => {
-      expect(multiply(2, '2')).toBe('ERROR: The \"num1\" parameter and the \"num2\" parameter must be of type number.');
+    describe('Invalid input tests.', () => {
+      test('Throws an error if the first number is of type string.', () => {
+        expect(() => subtract('2', 2)).toThrow('The "num1" parameter and the "num2" parameter must be of type number.');
+      });
+      test('Throws an error if the second number is of type string.', () => {
+        expect(() => subtract(2, '2')).toThrow('The "num1" parameter and the "num2" parameter must be of type number.');
+      });
+      test('Throws an error if one or more inputs are undefined.', () => {
+        expect(() => subtract(2)).toThrow('The "num1" parameter and the "num2" parameter must be of type number.');
+      });
     });
   });
-
-  describe('\"divide\" function tests', () => {
-    test('divide(2, 2) should return a value', () => {
-      expect(divide(2, 2)).toBeDefined();
+  describe('Multiply function tests.', () => {
+    describe('Valid input tests.', () => {
+      test('Multiply two positive integers.', () => {
+        expect(multiply(2, 2)).toBe(4);
+      });
+      test('Multiply a positive integer and a negative integer.', () => {
+        expect(multiply(2, -4)).toBe(-8);
+      });
+      test('Multiply an integer and a decimal number.', () => {
+        expect(multiply(0.2, 4)).toBe(0.8);
+      });
     });
-    test('divide(2, 2) should return a number', () => {
-      expect(typeof divide(2, 2)).toBe('number');
+    describe('Invalid input tests.', () => {
+      test('Throws an error if the first number is of type string.', () => {
+        expect(() => multiply('2', 2)).toThrow('The "num1" parameter and the "num2" parameter must be of type number.');
+      });
+      test('Throws an error if the second number is of type string.', () => {
+        expect(() => multiply(2, '2')).toThrow('The "num1" parameter and the "num2" parameter must be of type number.');
+      });
+      test('Throws an error if one or more inputs are undefined.', () => {
+        expect(() => multiply(2)).toThrow('The "num1" parameter and the "num2" parameter must be of type number.');
+      });
     });
-    test('divide(2, 2) should return the number 1', () => {
-      expect(divide(2, 2)).toBe(1);
+  });
+  describe('Divide function tests.', () => {
+    describe('Valid input tests.', () => {
+      test('Divide two positive integers.', () => {
+        expect(divide(2, 2)).toBe(1);
+      });
+      test('Divide a positive integer and a negative integer.', () => {
+        expect(divide(2, -4)).toBe(-0.5);
+      });
+      test('Divide an integer and a decimal number.', () => {
+        expect(divide(0.2, 4)).toBe(0.05);
+      });
     });
-    test('divide(2, -4) should return the number -0.5', () => {
-      expect(divide(2, -4)).toBe(-0.5);
-    });
-    test('divide(0.2, 4) should return the number 0.05', () => {
-      expect(divide(0.2, 4)).toBe(0.05);
-    });
-    test('divide(\'2\', 2) should return an error', () => {
-      expect(divide('2', 2)).toBe('ERROR: The \"num1\" parameter and the \"num2\" parameter must be of type number.');
-    });
-    test('divide(2, \'2\') should return an error', () => {
-      expect(divide(2, '2')).toBe('ERROR: The \"num1\" parameter and the \"num2\" parameter must be of type number.');
-    });
-    test('divide(2, 0 should return an error', () => {
-      expect(divide(2, 0)).toBe('ERROR: The \"num2\" parameter cannot have a value of 0.');
+    describe('Invalid input tests.', () => {
+      test('Throws an error if the first number is of type string.', () => {
+        expect(() => divide('2', 2)).toThrow('The "num1" parameter and the "num2" parameter must be of type number.');
+      });
+      test('Throws an error if the second number is of type string.', () => {
+        expect(() => divide(2, '2')).toThrow('The "num1" parameter and the "num2" parameter must be of type number.');
+      });
+      test('Throws an error if the second number is 0.', () => {
+        expect(() => divide(2, 0)).toThrow('The "num2" parameter cannot have a value of 0.');
+      });
+      test('Throws an error if one or more inputs are undefined.', () => {
+        expect(() => divide(2)).toThrow('The "num1" parameter and the "num2" parameter must be of type number.');
+      });
     });
   });
 });
