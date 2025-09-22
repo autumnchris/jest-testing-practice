8e3df228afb60ac637cf18d7971932ff7b4a2526
autumnchris <autumncbullard@gmail.com>
2025-09-22 16:22:16 -0400

    Refactor functions

--- calculator.js
+++ calculator.js
@@ -3,9 +3,8 @@ function add(num1, num2) {
   if (typeof num1 !== 'number' || typeof num2 !== 'number') {
     throw new Error('The "num1" parameter and the "num2" parameter must be of type number.');
   }
-  else {
-    return num1 + num2;
-  }
+  
+  return num1 + num2;
 }
 
 function subtract(num1, num2) {
@@ -13,9 +12,8 @@ function subtract(num1, num2) {
   if (typeof num1 !== 'number' || typeof num2 !== 'number') {
     throw new Error('The "num1" parameter and the "num2" parameter must be of type number.');
   }
-  else {
-    return num1 - num2;
-  }
+  
+  return num1 - num2;
 }
 
 function multiply(num1, num2) {
@@ -23,9 +21,8 @@ function multiply(num1, num2) {
   if (typeof num1 !== 'number' || typeof num2 !== 'number') {
     throw new Error('The "num1" parameter and the "num2" parameter must be of type number.');
   }
-  else {
-    return num1 * num2;
-  }
+  
+  return num1 * num2;
 }
 
 function divide(num1, num2) {
@@ -36,9 +33,8 @@ function divide(num1, num2) {
   else if (num2 === 0) {
     throw new Error('The "num2" parameter cannot have a value of 0.');
   }
-  else {
-    return num1 / num2;
-  }
+
+  return num1 / num2;
 }
 
 const calculator = {
--- reverse-string.js
+++ reverse-string.js
@@ -3,9 +3,8 @@ function reverseString(str) {
   if (typeof str !== 'string' || str.length === 0) {
     throw new Error('The "str" parameter must be of type string containing a value.');
   }
-  else {
-    return str.split('').reverse().join('');
-  }
+  
+  return str.split('').reverse().join('');
 }
 
 module.exports = reverseString;
